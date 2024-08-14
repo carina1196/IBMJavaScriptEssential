17a898e09ffee248c2ac0817cd5e0fcdc5ed0839
carina1196 <carinachanqingwei@gmail.com>
2024-08-14 11:41:57 +0800

    Updated weather app to make it work

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -7,12 +7,16 @@ function showWeatherDetails(event) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   fetch(apiUrl)
-    .then((response) => response.json)
+    .then((response) => {
+      return response.json();
+    })
     .then((data) => {
+      console.log(typeof data);
+
       const weatherInfo = document.getElementById("weatherInfo");
       weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
-                                 <p>Temperature: ${data.main.temp} &#8451;</p>
-                                 <p>Weather: ${data.weather[0].description}</p>`;
+                               <p> Temperature: ${data.main.temp}&deg;</p>
+                               <p>Weather: ${data.weather[0].description}</p>`;
     })
     .catch((error) => {
       console.error("Error fetching weather:", error);
